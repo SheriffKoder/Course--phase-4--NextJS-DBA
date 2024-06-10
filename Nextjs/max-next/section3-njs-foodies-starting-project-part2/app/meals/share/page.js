da7deeb34f503620fcdfae6173b31a4ba26e0207
SheriffKoder <102818968+SheriffKoder@users.noreply.github.com>
2024-06-10 16:49:02 +0300

    Input validation and production mode
    - Add server side input validation and related display errors
    - Production mode: Update cache by revalidating pages to display new added content using revalidatePath()

--- Nextjs/max-next/section3-njs-foodies-starting-project-part2/app/meals/share/page.js
+++ Nextjs/max-next/section3-njs-foodies-starting-project-part2/app/meals/share/page.js
@@ -1,10 +1,21 @@
+"use client" // because of useFormState
+
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import shareMeal from '@/lib/submit';
 
+// use the returned object from shareMeal function on invalid input
+import {useFormState} from 'react-dom';
+
 
 export default function ShareMealPage() {
+
+  // arguments (server action, initial state of this component)
+  // returns the current state of this form, which will be null or the returned object of message when an error occurs on shareMeal
+  // formAction, value for the form action prop which will be the shareMeal we used directly before
+  // useFormState state will pass another parameter before formData to shareMeal
+  const [state, formAction] = useFormState(shareMeal, {message: null});
 
 
 
@@ -18,11 +29,13 @@ export default function ShareMealPage() {
       </header>
       <main className={classes.main}>
         {/* behind the scenes, create a request and send it to the next.js server serving the website and trigger this function on the server as defined in it  */}
-        <form className={classes.form} action={shareMeal}>
+        {/* <form className={classes.form} action={shareMeal}> */}
+        <form className={classes.form} action={formAction}>
+
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name"  />
             </p>
             <p>
               <label htmlFor="email">Your email</label>
@@ -47,6 +60,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             {/* <button type="submit">Share Meal</button> */}
             <MealsFormSubmit />
--- Nextjs/max-next/section3-njs-foodies-starting-project-part2/lib/submit.js
+++ Nextjs/max-next/section3-njs-foodies-starting-project-part2/lib/submit.js
@@ -5,7 +5,8 @@ import { saveMeal } from "./meals";
 
   // handle form submit
   // object received from the form containing information
-  const shareMeal = async (formData) => {
+  // useFormState state will pass another parameter before formData to shareMeal
+  const shareMeal = async (prevState, formData) => {
     // async + "use server" + action attribute on the form element
     // creates a server action, guarantee that this function executes on the server side
     // "use server" // not used as it is defined at top of the file
@@ -18,11 +19,33 @@ import { saveMeal } from "./meals";
       image: formData.get("image"),
       creator: formData.get("name"),
       creator_email: formData.get("email"),
+    };
+
+    // input validation
+    function isInvalidText (text) {
+        return !text || text.trim() === "";
+    }
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes("@") ||
+        !meal.image || meal.image.size === 0
+    ) {
+        // throw new Error ("Invalid Input");
+        return {
+            message: "Invalid input."
+        }
     }
 
     // console.log(meal);
     await saveMeal(meal);
-    
+    // revalidate the cache for /meals and layout (which includes also nested pages will view the new meal) 
+    // to avoid using the previous cached data even after update on the webpage in production/build mode
+    revalidatePath("/meals", "layout"); 
     redirect("/meals");
 
     
