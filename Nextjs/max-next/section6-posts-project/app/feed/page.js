20190a74843f1221c4ea547ce1e0a6a6ebd8bb10
SheriffKoder <102818968+SheriffKoder@users.noreply.github.com>
2024-07-19 04:59:54 +0300

    Add static, dynamic and layout metadata

--- Nextjs/max-next/section6-posts-project/app/feed/page.js
+++ Nextjs/max-next/section6-posts-project/app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// Dynamic metadata
+// data or any other name, automatically passed by nextjs or {params} if it is a dynamic page
+export async function generateMetadata(data) {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `Browser all our ${numberOfPosts} posts`,
+    description: "Browse all out posts"
+  }
+
+}
+
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
--- Nextjs/max-next/section6-posts-project/app/page.js
+++ Nextjs/max-next/section6-posts-project/app/page.js
@@ -3,6 +3,12 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// Static metadata
+export const metadata = {
+  title: "Latest posts",
+  description: "Browse our latest posts"
+}
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
